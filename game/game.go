f70397741c65e401aec14669e61c4cabe4995c5c
Daniil Danilov <k-ddanilo@c3r4p5.hive.fi>
2026-02-04 13:30:51 +0200

    Add funcs to remove input letters from alphabet

--- game/game.go
+++ game/game.go
@@ -1,12 +1,39 @@
 package game
 
+const word1 string = "hello"
+const word2 string = "admin"
+const word3 string = "amigo"
 
-
-func Letters() []string {
+func Letters() string {
 	var i int = 65
-	var answer []string
+	var answer string
 	for ; i <= 90; i++ {
-		answer = append(answer, string(rune(i)))
+		answer += string(rune(i))
 	}
 	return answer
+}
+
+func CheckLetters(input string) string {
+	var check string
+	for _, v := range input {
+		check += string(v - 'a' + 'A')
+
+	}
+	return check
+}
+
+func UpdateLetters(word string) string {
+	old := Letters()
+	new := CheckLetters(word)
+	var newAnswer string
+
+	for i := 0; i < len(old); i++ {
+		for _, v := range new {
+			if rune(old[i]) == v {
+				i++
+			}
+		}
+		newAnswer += string(rune(old[i]))
+	}
+	return newAnswer
 }
