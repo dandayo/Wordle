765693c5cdfd05be5cedbd54c410512006e06f31
Daniil Danilov <k-ddanilo@c3r4p5.hive.fi>
2026-02-04 17:18:22 +0200

    Delete usage letters from alphabet

--- input/input.go
+++ input/input.go
@@ -16,3 +16,38 @@ func Input() string {
     }
     return line
 }
+
+func Letters() string {
+	var i int = 65
+	var answer string
+	for ; i <= 90; i++ {
+		answer += string(rune(i))
+	}
+	return answer
+}
+
+var alphabet string = Letters()
+
+func CheckLetters(input string) string {
+	var check string
+	for _, v := range input {
+		check += string(v - 'a' + 'A')
+
+	}
+	return check
+}
+
+func UpdateLetters(input string) string {
+	new := CheckLetters(input)
+	var newAnswer string
+	for i := 0; i < len(alphabet); i++ {
+		for _, v := range new {
+			if rune(alphabet[i]) == v {
+				i++
+			}
+		}
+		newAnswer += string(rune(alphabet[i]))
+	}
+	alphabet = newAnswer
+	return alphabet
+}
